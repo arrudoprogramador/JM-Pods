aa4789ed47853577c44701d1b889763bd33da93f
Arruda <>
2026-03-14 13:53:23 -0300

    primeiro commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,39 @@
     </div>
   </div>
 </nav>
+
+      <div class="d-flex justify-content-center gap-3">
+        
+        <div class="card" style="width: 18rem;">
+          <img src="{{ asset('img/pod (1).jpg') }}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Pod 1</h5>
+            <p class="card-text">Descrição do produto</p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+          <img src="{{ asset('img/pod (2).jpg') }}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Pod 2</h5>
+            <p class="card-text">Descrição do produto</p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+          <img src="{{ asset('img/pod (2).jpg') }}" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Pod 3</h5>
+            <p class="card-text">Descrição do produto</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
